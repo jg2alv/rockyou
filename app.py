2ba1d6fb03a41b3a05eff1ea9e797522678e1660
João <joao.guimaraes.alves.vaz@gmail.com>
2021-02-10 14:20:22 -0300

    Minor changes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import argparse
-import itertools
 import os
-import sys
 import re
+import sys
 
-import utils
 import rockyou
+import utils
+
 
 def parse_args():
     abspath = os.path.abspath(os.path.dirname(__file__))
@@ -14,7 +14,8 @@ def parse_args():
         description='Combination-based password generator')
     parser.add_argument('-p', '--path', type=str, default=abspath,
                         help=f'Path to store password file on (without filename). Defaults to {abspath}')
-    parser.add_argument('-f', '--file', type=str, default='rockyou.txt', help='File name with extension. Defaults to rockyou.txt')
+    parser.add_argument('-f', '--file', type=str, default='rockyou.txt',
+                        help='File name with extension. Defaults to rockyou.txt')
     parser.add_argument('-l', '---disable-lowercase', action='store_true',
                         help='If given, will not include lowercase letters on character set')
     parser.add_argument('-U', '---disable-uppercase', action='store_true',
@@ -32,7 +33,7 @@ def parse_args():
     parser.add_argument('-M', '---maximum-length', type=int, default=16,
                         help='Maximum length of passwords (including the given number). Defaults to 16')
     parser.add_argument('-s', '--separator', type=str, default=',',
-                        help=r'Separator of combinations. Use "\n" to use a linebreak as a separator. Defaults to a comma')
+                        help="Separator of combinations. Use '\\n' to use a linebreak as a separator. Defaults to a comma")
     parser.add_argument('---estimate-size', action='store_true',
                         help='Will print the esitmated end-file size and exit')
     parser.add_argument('---estimate-time', action='store_true',
@@ -63,13 +64,19 @@ def parse_args():
         args.character_set = list(filter(
             lambda c: not c in args.disable_chars, args.character_set))
 
+    if args.separator in args.character_set:
+        parser.print_usage()
+        print('app.py: error: argument -s/--separator: character set cannot contain separator character')
+        sys.exit(-1)
+
+    args.path += '/' if args.path[-1] != '/' else args.path
+    args.separator.replace('\\n', '\n').replace('\\t', '\t')
+
     return args
 
 
 def main() -> int:
     args = parse_args()
-    args.path = args.path[0:-1] if args.path[-1] == '/' else args.path
-    args.separator = '\n' if args.separator == r'\n' else args.separator
     estimator = utils.Estimator(args)
 
     if args.estimate:
@@ -83,7 +90,8 @@ def main() -> int:
             return print(estimator.estimate_ammount()) or 0
 
     with open(f'{args.path}/{args.file}', 'wb+') as f:
-        [ f.write(bytes(utils.join(c, args.separator), 'utf-8')) for c in rockyou.rockyou(args.minimum_length, args.maximum_length, args.character_set) ]
+        [f.write(bytes(utils.join(c, args.separator), 'utf-8'))
+         for c in rockyou.rockyou(args.minimum_length, args.maximum_length, args.character_set)]
 
         f.seek(-1, os.SEEK_END)
         f.truncate()
